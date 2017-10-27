5de7fc9df7801f763b572e114f389dc96740c4b8
Mark Peng <markpeng@exosite.com>
2017-10-27 10:53:02 +0800

    Added camera settings and increase brightness.

--- webcam-pi.py
+++ webcam-pi.py
@@ -42,6 +42,25 @@ cap_height = 240
 
 # initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
+
+camera.brightness = 70
+camera.sharpness = 0
+camera.contrast = 0
+camera.brightness = 50
+camera.saturation = 0
+camera.ISO = 0
+camera.video_stabilization = False
+camera.exposure_compensation = 0
+camera.exposure_mode = 'auto'
+camera.meter_mode = 'average'
+camera.awb_mode = 'auto'
+camera.image_effect = 'none'
+camera.color_effects = None
+camera.rotation = 0
+camera.hflip = False
+camera.vflip = False
+camera.crop = (0.0, 0.0, 1.0, 1.0)
+
 camera.resolution = (640, 480)
 rawCapture = PiRGBArray(camera, size=(640, 480))
 
